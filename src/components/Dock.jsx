f2010093f0794729ecacf676c6447848ff434905
Anasse <anasse@MacBook-Pro-de-Anasse.local>
2024-05-17 18:43:11 +0100

    Update Dock's Icons

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useRef } from 'react'
 import { motion, useMotionValue, useSpring, useTransform, } from "framer-motion";
-import launchpad from '../assets/dock/launchpad-icon.png'
-import photos from '../assets/dock/Potos.svg'
-import FaceTime from '../assets/dock/FaceTime.png'
 
-import systemPreferences from '../assets/dock/SoftwareUpdate.png'
+import launchpad from '../assets/dock/Launchpad.png'
+import photos from '../assets/dock/Photos.png'
+import FaceTime from '../assets/dock/FaceTime.png'
+import systemPreferences from '../assets/dock/systemPreferences.png'
 import Finder from '../assets/dock/FinderIcon.png'
 import DownloadsFolder from '../assets/dock/DownloadsFolder.png'
 import FullTrashIcon from '../assets/dock/FullTrashIcon.png'
 import TrashIcon from '../assets/dock/TrashIcon.png'
+import AppStore from '../assets/dock/AppStore.png'
+import Calculator from '../assets/dock/Calculator.png'
+import Contacts from '../assets/dock/Contacts.png'
+import Messages from '../assets/dock/Messages.png'
 
 const icons = [
     {
@@ -26,6 +30,22 @@ const icons = [
     {
         name: 'FaceTime',
         icon: FaceTime
+    },
+    {
+        name: 'Messages',
+        icon: Messages
+    },
+    {
+        name: 'Calculator',
+        icon: Calculator
+    },
+    {
+        name: 'Contacts',
+        icon: Contacts
+    },
+    {
+        name: 'App Store',
+        icon: AppStore
     },
     {
         name: 'System Preferences',
@@ -51,7 +71,7 @@ const DockIcon = ({ mouseX, icon }) => {
     });
 
     let widthSync = useTransform(distance, [-150, 0, 150], [50, 90, 50]);
-    let width = useSpring(widthSync, { mass: 0.1, stiffness: 150, damping: 12 });
+    let width = useSpring(widthSync, { mass: 0.1, stiffness: 300, damping: 12 });
 
     return (
         <motion.div
