f9bbfdb9e9b96af38a871396bc575a005b9f4897
Anasse <anasse@MacBook-Pro-de-Anasse.local>
2024-05-17 12:13:16 +0100

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sanfranciscoLight: ['sanfranciscoLight'],
+        sanfranciscoRegular: ['sanfranciscoRegular'],
+        sanfranciscoMedium: ['sanfranciscoMedium'],
+        sanfranciscoBold: ['sanfranciscoBold']
+      }
+    },
   },
   plugins: [],
 }
